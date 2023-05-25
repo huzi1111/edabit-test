d900748542207871631aff18951e1186bb4e9475
huzi1111 <huzaifaliaquat@gmail.com>
2023-05-25 14:25:31 +0500

    reversing a string

---
+++ easy/largestSwap.js
@@ -0,0 +1,44 @@
+const { Test } = require("../utils");
+
+function largestSwap(num) {
+let firstnum = Math.floor(num/10);
+let secnum = (num%10);
+result = ("" + secnum + firstnum);
+if(num > result)
+    {
+        return true;
+    }
+   else if(num == result)
+    {
+        return true;
+    }
+    return false;
+
+
+// while(num>0){
+//     // Getting the rightmost digit
+//    let rightmost = num%10;
+    
+//     result = result*10 + rightmost;
+    
+//     // Removing the rightmost digit from the number
+//     num = Math.floor(num/10);
+//     if(num > result)
+//     {
+//         return true;
+//     }
+//     else if(num == result)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+}
+
+Test.assertEquals(largestSwap(27), false, '27 < 72, so not largest swap.')
+Test.assertEquals(largestSwap(43), true, '43 > 34, so largest swap.')
+Test.assertEquals(largestSwap(14), false, '14 < 41, so not largest swap.')
+Test.assertEquals(largestSwap(53), true, '53 > 35, so largest swap.')
+Test.assertEquals(largestSwap(99), true, 'Cannot do better, so largest swap.')
+
+Test.getSummary();
